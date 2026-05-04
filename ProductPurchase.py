642e401975c5870c26db0d51cecad84dc7474b6d
Sanika Patil <sanika212005>
2026-05-04 20:48:59 +0530

    Added Product info

--- ProductPurchase.py
+++ ProductPurchase.py
@@ -1,7 +1,14 @@
+ProductName=input("Name of the Product:")
 MRP=float(input("Enter the MRP of the product:"))
 Discount_in_percent=float(input("Enter thee Discount on the Product:"))
 Discount_in_rupees=MRP*(Discount_in_percent/100)
 Selling_price=MRP-Discount_in_rupees
-print("Selling price of the product:",Selling_price)
+
+#displaying the Product  data 
+print("")
+print("Name of the Product:",ProductName)
+print("MRP of the Product:",MRP)
+print("Discount on the Product",Discount_in_percent)
+print("Selling price of the Product:",Selling_price)
 
  

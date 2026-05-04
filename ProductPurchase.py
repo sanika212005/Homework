610336b30a4fe11d8ebb4f5c601d77942a741968
Sanika Patil <sanika212005>
2026-05-04 20:19:01 +0530

    small project created on 4th may

---
+++ ProductPurchase.py
@@ -0,0 +1,7 @@
+MRP=float(input("Enter the MRP of the product:"))
+Discount_in_percent=float(input("Enter thee Discount on the Product:"))
+Discount_in_rupees=MRP*(Discount_in_percent/100)
+Selling_price=MRP-Discount_in_rupees
+print("Selling price of the product:",Selling_price)
+
+ 
